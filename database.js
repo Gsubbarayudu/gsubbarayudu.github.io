39e5eb3a293907c1cfa8d0fc99252ca451a2b9cc
GANDIKOTA SUBBARAYUDU <133373105+Gsubbarayudu@users.noreply.github.com>
2026-02-21 15:35:05 +0530

    Add files via upload

---
+++ database.js
@@ -0,0 +1,43 @@
+// This file simulates a JSON database of student records.
+const studentDatabase = [
+  {
+    id: "AR001",
+    name: "Avinash Royal",
+    course: "Full Stack Development",
+    date: "September 15, 2025",
+    issuer: "Subbarayudu",
+    hasCertificate: true, // true = PDF has been uploaded
+  },
+  {
+    id: "SR002",
+    name: "Amit Singh",
+    course: "Java Development",
+    date: "August 28, 2025",
+    issuer: "Anjali Gupta",
+    hasCertificate: false,
+  },
+  {
+    id: "SRPYI521",
+    name: "B.PAVAN KUMAR",
+    course: "Python Development",
+    date: "August 20, 2025",
+    issuer: "Avinash Royal",
+    hasCertificate: true,
+  },
+  {
+    id: "SRFSI501",
+    name: "GANDIKOTA SUBBARAYUDU",
+    course: "Full Stack Development",
+    date: "July 25, 2025",
+    issuer: "Avinash Royal",
+    hasCertificate: true, // false = PDF has NOT been uploaded yet
+  },
+  {
+    id: "SRAI26501",
+    name: "ARAVA CHARAN TEJA",
+    course: "ARTIFICIAL INTELLIGENCE",
+    date: "Jan 31, 2026",
+    issuer: "Avinash Royal",
+    hasCertificate: true, // false = PDF has NOT been uploaded yet
+  },
+];
